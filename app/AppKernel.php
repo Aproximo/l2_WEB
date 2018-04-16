12246504afd7c0a9ba81a9ef83049f6252a39519
Work\Romaniuk.O <Romanyk94@gmail.com>
2018-04-16 14:14:28 +0300

    add reCaptcha

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
+            new LoginRecaptcha\Bundle\LoginRecaptchaBundle(),
 //            new FOS\UserBundle\FOSUserBundle(),
         ];
 

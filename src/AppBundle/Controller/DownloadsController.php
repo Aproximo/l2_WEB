4ef506ff49606eb00e767e275174bb6921bafbb0
Vladislav Zolotaryov <zolotaryov.v.g@gmail.com>
2018-04-23 12:00:39 +0300

    Lang change

--- src/AppBundle/Controller/DownloadsController.php
+++ src/AppBundle/Controller/DownloadsController.php
@@ -10,14 +10,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DownloadsController extends Controller
 {
     /**
-     * article list page
      *
      * @Route("/downloads", name="downloads")
      * @Template()
      */
-    public function indexAction(){
-
-
+    public function indexAction() {
         return [];
     }
 
